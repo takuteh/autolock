c3bad44fdd1d610fa4e2cf8ac30d890601891f72
takuteh <takumi.ktym@gmail.com>
2024-02-10 10:58:00 +0900

    pigpioでサーボモーター制御を書き直した

---
+++ C/src/autolock/down_close.c
@@ -0,0 +1,16 @@
+#include <pigpiod_if2.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SERVO_PIN 6
+int main(){
+int pi;
+        //pigpio初期化
+ pi=pigpio_start(0,0);
+set_servo_pulsewidth(pi,SERVO_PIN,1500);
+sleep(2);
+set_servo_pulsewidth(pi,SERVO_PIN,500);
+sleep(2);
+ set_servo_pulsewidth(pi,SERVO_PIN,1500);
+return 0;
+}
